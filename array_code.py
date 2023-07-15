c346e5f6f3aa0f3e6ada0286bbba7469e5364667
NishaKadam2808 <139627452+NishaKadam2808@users.noreply.github.com>
2023-07-15 17:47:10 +0530

    Array

---
+++ array_code.py
@@ -0,0 +1,82 @@
+#printng array
+'''import array as arr
+a=arr.array('i',[20,30,40])
+print(a)'''
+
+#looping array
+'''import array as arr
+a=arr.array('i',[20,30,40])
+for b in a:
+    print(b)'''
+
+#unsigned
+'''import array as arr
+u=arr.array('I',[20,30,40])
+print(u)'''
+
+#adding ele.
+'''import array as arr
+a=arr.array('i',[20,30,40])
+sum=0
+for b in a:
+    sum=sum+b
+print(sum)'''
+
+#character array
+'''import array as arr
+a=arr.array('u',['n','k'])
+print(a)'''
+
+'''import array as arr
+a=arr.array('u',['n','k'])
+print(type(a))'''
+
+'''import array as arr
+a=arr.array('f',[20.6,30.7,40.9])
+print(a)'''
+
+#arraytype attribute
+'''import array as arr
+print(arr.ArrayType('i'))'''
+
+import array as arr
+a=arr.array('i',[20,30,40])
+for b in a:
+    max=b
+    if()
+print(max(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
