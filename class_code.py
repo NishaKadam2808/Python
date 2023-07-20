fdd78d1e977650bac17f26fc4c172fc108f68ce6
NishaKadam2808 <139627452+NishaKadam2808@users.noreply.github.com>
2023-07-20 21:20:52 +0530

    Class

---
+++ class_code.py
@@ -0,0 +1,26 @@
+#print
+'''class demo():
+    
+    def method1(self):
+        print("Hii")
+    def method2(self):
+        print("This is method2 from class demo")
+d1=demo()
+d1.method1()
+d1.method2()'''
+
+#add
+'''class demo():
+    
+    def method1(self):
+        n1=int(input("Enter 1 : "))
+        n2=int(input("Enter 2 : "))
+        print(n1+n2)
+    
+d1=demo()
+d1.method1()'''
+
+
+
+
+
