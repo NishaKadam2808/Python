26d31342096381354e14ba29ad50d72806d16234
NishaKadam2808 <139627452+NishaKadam2808@users.noreply.github.com>
2023-07-23 19:37:27 +0530

    Lambda

---
+++ lambda.py
@@ -0,0 +1,21 @@
+#squaring list ele. using map
+'''lst=[5,9,8,2]
+sqr=list(map(lambda i :(i*i),lst))
+print(sqr)'''
+
+#squaring tuple ele. using map
+'''tpl=(5,9,8,2)
+sqr=tuple(map(lambda i :(i*i),tpl))
+print(sqr)'''
+
+#squaring dictionary ele. using map
+'''dic={1:"nisha",2:"nk"}
+lst=list(dic)
+sqr=list(map(lambda i :(i*i),dic))
+print(sqr)'''
+
+#filter
+'''lst=[3,1,4]
+even=list(filter(lambda i :(i%2==0),lst))
+print(even)'''
+
